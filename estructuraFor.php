e35272c533af478c1687983fb4495e2eedec09a1
esecharly <62493210+esecharly@users.noreply.github.com>
2021-03-25 02:28:30 -0600

    Create estructuraFor.php

---
+++ estructuraFor.php
@@ -0,0 +1,15 @@
+<?php
+    //es una estructura de control iterativa con inicio, final y contador
+
+    for($i = 0; $i < 10; $i ++){
+        //echo $i;
+        //echo "<br>";
+    }
+
+    //tabla del 10 for
+
+    for ($i=1; $i < 11; $i++) { 
+        echo "10 x ".$i."=".$i*10;
+        echo "<br>";
+    }
+?>
