4eecc668fb6ba6c4f6cf2f9995315f6f06d5d862
esecharly <62493210+esecharly@users.noreply.github.com>
2021-03-25 02:19:50 -0600

    Create sentenciaCase.php

---
+++ sentenciaCase.php
@@ -0,0 +1,17 @@
+<?php
+    $valorMaestro = 'erere';
+
+    switch($valorMaestro){
+        case 'A': 
+            echo "este es el caso 1";
+            break;
+
+        case 2:
+            echo "este es el caso 2";
+            break;
+
+        default:
+            echo "no existe ese caso";   
+            break;     
+    }
+?>
