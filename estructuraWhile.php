254abc462248eb644ebb34bfddb563526188acf3
esecharly <62493210+esecharly@users.noreply.github.com>
2021-03-25 02:34:42 -0600

    Create estructuraWhile.php

---
+++ estructuraWhile.php
@@ -0,0 +1,17 @@
+<?php
+    //es una estructura de control iterativa que funciona siempre y cuando una condicion sea valida
+
+    $condicion = 0;
+
+    while ($condicion < 10) {
+        echo "se cumple la condicion";
+        echo "<br>";
+        echo $condicion++;
+        echo "<br>";
+    }
+
+    //while infinito
+    while (true) {
+        # code...
+    }
+?>
