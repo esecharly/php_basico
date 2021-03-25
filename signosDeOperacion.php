8152a539f6f506129098699673e9d695d6f917b9
esecharly <62493210+esecharly@users.noreply.github.com>
2021-03-25 01:31:21 -0600

    Create signosDeOperacion.php

---
+++ signosDeOperacion.php
@@ -0,0 +1,25 @@
+<?php
+
+    //para concatenar se usa el . 
+
+    //declaracion de variables
+    $valor1 = 2;
+    $valor2 = 4;
+    $valor3 = 5;
+
+    $resultado = $valor1 + $valor2 + $valor3;
+
+    //signos de operaciones
+
+    //para suma +
+    //para resta -
+    //para multiplicacion *
+    //para division /
+
+    //ejemplo de concatenacion
+    $nombre = "pepe";
+    $apellido = "lopez";
+    echo $nombre." ".$apellido." ".$resultado;
+
+
+?>
