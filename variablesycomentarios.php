80e8e150d607338cffcfd53c24b24c96e108fced
esecharly <62493210+esecharly@users.noreply.github.com>
2021-03-25 01:19:46 -0600

    Create variablesycomentarios.php

---
+++ variablesycomentarios.php
@@ -0,0 +1,11 @@
+<?php
+    //Declaracion de variables
+    $mivariable = 1;
+    $mivariable2 = "nombre";
+    $mivariable3 = 1.5;
+
+    //Mostrar informacion en pantalla
+    echo $mivariable2;
+    echo "<br>";
+    print $mivariable3;
+?>
