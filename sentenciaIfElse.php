f8d96829685f0fd4eff8c02bae43a2e7dac3def3
esecharly <62493210+esecharly@users.noreply.github.com>
2021-03-25 01:46:35 -0600

    Create sentenciaIfElse.php

---
+++ sentenciaIfElse.php
@@ -0,0 +1,14 @@
+<?php
+    //estructura de control para validar de una forma u otra
+
+    $mivalor = true;
+
+    $valor1 = 5;
+    $valor2 = 10;
+
+    if($valor1 > $valor2){
+        echo "es verdadero";
+    }else {
+        echo "es falso";
+    }
+?>
