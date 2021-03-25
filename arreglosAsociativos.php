f7aabb44c3d5c3946ef9ea1920d4368912a22711
esecharly <62493210+esecharly@users.noreply.github.com>
2021-03-25 02:56:14 -0600

    Create arreglosAsociativos.php

---
+++ arreglosAsociativos.php
@@ -0,0 +1,12 @@
+<?php
+    //es un tipo de dato que pude almacenar n datos pero en vez de guardar por indice se proporciona una llave
+
+    $arregloColores = array(
+        "coche" => "verde",
+        "moto" => "roja",
+        "avion" => "gris"
+    );
+
+    //var_dump($arregloColores);
+    echo $arregloColores['coche'];
+?>
