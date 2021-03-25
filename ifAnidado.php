0c7952ecdb289b8e66232764f60cae8f4d671cdd
esecharly <62493210+esecharly@users.noreply.github.com>
2021-03-25 01:56:18 -0600

    Create ifAnidado.php

---
+++ ifAnidado.php
@@ -0,0 +1,15 @@
+<?php
+    //es una estructura de control que te permite validar multiples casos a la vez
+
+    $edad = 25;
+
+    if($edad == 5){
+        echo "es un niño";
+    }else if($edad == 15){
+        echo "es un puberto";
+    }else if($edad == 20){
+        echo "es un adulto joven";
+    }else{
+        echo "no se que es";
+    }
+?>
