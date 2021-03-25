d3c7377139ecbb66113403b7f37368a23563230b
esecharly <62493210+esecharly@users.noreply.github.com>
2021-03-25 01:39:47 -0600

    Create sentenciaIf.php

---
+++ sentenciaIf.php
@@ -0,0 +1,22 @@
+<?php
+    //la sentencia if sirve para validacion de sentencias
+
+    $numero1 = 10;
+    $numero2 = 10;
+
+    //signo mayor o menor para comparar numeros
+
+    //menor o igual y tambien mayor o igual
+
+    //declaracion de variables tipo boolean
+
+    $bandera = 0;
+
+    if($numero1 >= $numero2){
+        echo "si es verdad";
+    }
+
+    /* if($bandera){
+        echo "verdadero";
+    }*/
+?>
